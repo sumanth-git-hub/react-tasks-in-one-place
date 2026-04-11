1263fd51fb8ad53f84de525c1ad1050f2f6f0940
Sumanth <sumanth@Sumanths-MacBook-Air.local>
2026-04-11 23:41:51 +0530

    updated the todoList file

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useTheme } from "../hooks/UseTheme";
+import { useTheme } from "../hooks/useTheme";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import {
   closestCorners,
